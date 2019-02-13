eb11d551fa596edfed6377cf9465115c4cf1dab1
wym6912 <wym6912@outlook.com>
2019-02-13 22:37:24 +0800

    Test Install IEEE State

--- models/contest_player.js
+++ models/contest_player.js
@@ -49,7 +49,7 @@ class ContestPlayer extends Model {
 
   async updateScore(judge_state) {
     await this.loadRelationships();
-    if (this.contest.type === 'ioi') {
+    if (this.contest.type === 'ioi' || this.contest.type === 'ieee') { //以后在这里修改 IEEE 赛制
       if (!judge_state.pending) {
         if (!this.score_details[judge_state.problem_id]) {
           this.score_details[judge_state.problem_id] = {
